ba36d3fbaca9b78c0b4ed05c55df8c023a783765
Muhammed Abbas <muhammedabbas@Muhammeds-MacBook-Pro.local>
2025-03-13 23:03:10 -0400

    fetch the list of services from the backend via rest API

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,8 @@
 from application import app
 from application.resources.wash_services_resource import wash_service_bp
+from flask_cors import CORS
+
+CORS(app)
 
 app.register_blueprint(wash_service_bp)
 if __name__ == '__main__':
--- frontend/assets/js/load-services.js
+++ frontend/assets/js/load-services.js
@@ -8,7 +8,7 @@ window.addEventListener('load', async function () {
     }
 
     try {
-        const response = await fetch('assets/data/services.json');
+        const response = await fetch('http://127.0.0.1:5000/wash_services');
         if (!response.ok) throw new Error('Failed to load services.json');
 
         const services = await response.json();
@@ -16,13 +16,15 @@ window.addEventListener('load', async function () {
             const serviceItem = document.createElement('div');
             serviceItem.className = `col-lg-6`;
             serviceItem.setAttribute('data-aos', 'fade-up');
-            serviceItem.setAttribute('data-aos-delay', service.delay);
+            let delay = 100;
+            serviceItem.setAttribute('data-aos-delay', delay);
+            delay += 100;
 
             serviceItem.innerHTML = `
                 <div class="service-item d-flex">
-                    <div class="icon flex-shrink-0"><i class="${service.icon}"></i></div>
+                    <div class="icon flex-shrink-0"><i class="${service.images_path}"></i></div>
                     <div>
-                        <h4 class="title"><a href="${service.link}" class="stretched-link">${service.title}</a></h4>
+                        <h4 class="title"><a href="" class="stretched-link">${service.type}</a></h4>
                     </div>
                 </div>
             `;
