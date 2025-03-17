26192877008df3040b11e6057d4a6bfda1da59c5
Muhammed Abbas <muhammedabbas@Muhammeds-MacBook-Pro.local>
2025-03-17 02:10:21 -0400

    add the home page sections, fix stylings and bugs

--- frontend/assets/js/load-sections.js
+++ frontend/assets/js/load-sections.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', async () => {
     // Map of container IDs and corresponding section files
     const sections = {
-        servicesList: '../sections/services-list.html',  // Load the "services-list.html" into services-list
         header: '../components/header.html',    // Load the "home.html" into header
+        hero: '../sections/hero.html',  // Load the "hero.html" into hero
+        about: '../sections/about.html',  // Load the "about.html" into about
+        stats: '../sections/stats.html',  // Load the "stats.html" into stats
+        servicesList: '../sections/services-list.html',  // Load the "services-list.html" into services-list
+        features: '../sections/features.html',  // Load the "features.html" into features
         footer: '../components/footer.html', // Load the "footer.html" into footer
-        portfolioSec: '../pages/portfolio-details.html', // Load the "portfolio-details.html" into portfolioSec
+        portfolio: '../sections/portfolio.html', // Load the "portfolio.html" into portfolio
+        callToAction: '../sections/call-to-action.html', // Load the "call-to-action.html" into call-to-action
+        breadcrumb: '../components/breadcrumb.html', // Load the "breadcrumb.html" into breadcrumb
     };
 
     // Function to load a section
